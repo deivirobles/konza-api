12db960e8e36ff16bbc099ca8b722d03b29cc40d
Deivi Robles <drozam24@gmail.com>
2019-08-14 10:43:16 -0500

    Define REST API contract for tasks resource

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 const tasks = require('./tasks/routes');
 
-router.route('/tasks').get((req, res, next) => {
-  res.json({
-    message: 'GET all tasks',
-  });
-});
-
 router.use('/tasks', tasks);
 
 module.exports = router;
--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-  res.json({
-    message: 'GET all tasks',
-  });
-});
+/*
+ * /api/tasks/ POST - CREATE
+ * /api/tasks/ GET - READ ALL
+ * /api/tasks/:id GET - READ ONE
+ * /api/tasks/:id PUT - UPDATE
+ * /api/tasks/:id DELETE - DELETE
+ */
+
+router
+  .route('/')
+  .post((req, res, next) => {})
+  .get((req, res, next) => {});
+
+router
+  .route('/:id')
+  .get((req, res, next) => {})
+  .put((req, res, next) => {})
+  .delete((req, res, next) => {});
 
 module.exports = router;
