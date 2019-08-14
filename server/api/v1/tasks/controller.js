2d1687a9773bd1de355414ec27c7bdf6dacd46a0
Deivi Robles <drozam24@gmail.com>
2019-08-14 14:00:50 -0500

    Refactor callback to Promises in Tasks controller

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -3,49 +3,53 @@ const HTTP_STATUS_CODE = require('http-status-codes');
 const Model = require('./model');
 
 exports.id = (req, res, next, id) => {
-  Model.findById(id).exec((err, doc) => {
-    if (err) {
+  Model.findById(id)
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        req.doc = doc;
+        next();
+      } else {
+        next({
+          message: 'Resource not found',
+          statusCode: HTTP_STATUS_CODE.NOT_FOUND,
+        });
+      }
+    })
+    .catch((err) => {
       next(err);
-    } else if (doc) {
-      req.doc = doc;
-      next();
-    } else {
-      next({
-        message: 'Resource not found',
-        statusCode: HTTP_STATUS_CODE.NOT_FOUND,
-      });
-    }
-  });
+    });
 };
 
 exports.create = (req, res, next) => {
   const { body = {} } = req;
-  Model.create(body, (err, doc) => {
-    if (err) {
-      next(err);
-    } else {
+  Model.create(body)
+    .then((doc) => {
       res.status(HTTP_STATUS_CODE.CREATED);
       res.json({
         data: doc,
         success: true,
         statusCode: HTTP_STATUS_CODE.CREATED,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.all = (req, res, next) => {
-  Model.find().exec((err, docs) => {
-    if (err) {
-      next(err);
-    } else {
+  Model.find()
+    .exec()
+    .then((docs) => {
       res.json({
         data: docs,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.read = (req, res, next) => {
@@ -60,30 +64,33 @@ exports.read = (req, res, next) => {
 exports.update = (req, res, next) => {
   const { body = {}, doc } = req;
   Object.assign(doc, body);
-  doc.save((err, updated) => {
-    if (err) {
-      next(err);
-    } else {
+
+  doc
+    .save()
+    .then((updated) => {
       res.json({
         data: updated,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.delete = (req, res, next) => {
   const { doc } = req;
-  doc.remove((err, deleted) => {
-    if (err) {
-      next(err);
-    } else {
+  doc
+    .remove()
+    .then((deleted) => {
       res.json({
         data: deleted,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
