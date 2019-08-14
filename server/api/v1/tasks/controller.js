78d12ff77d7078c402c53bff0c6d6f289d77ea26
Deivi Robles <drozam24@gmail.com>
2019-08-14 11:33:53 -0500

    Standarize response

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -1,33 +1,62 @@
+const HTTP_STATUS_CODE = require('http-status-codes');
+
 exports.create = (req, res, next) => {
   const { body = {} } = req;
-  res.json(body);
+  const doc = body;
+
+  res.status(HTTP_STATUS_CODE.CREATED);
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.CREATED,
+  });
 };
 
 exports.all = (req, res, next) => {
-  res.json([]);
+  const docs = [];
+  res.json({
+    data: docs,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
+  });
 };
 
 exports.read = (req, res, next) => {
   const { params = {} } = req;
   const { id } = params;
-  res.json({
+  const doc = {
     id,
+  };
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
   });
 };
 
 exports.update = (req, res, next) => {
   const { body = {}, params = {} } = req;
   const { id } = params;
-  res.json({
+  const doc = {
+    ...body,
     id,
-    body,
+  };
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
   });
 };
 
 exports.delete = (req, res, next) => {
   const { params = {} } = req;
   const { id } = params;
-  res.json({
+  const doc = {
     id,
+  };
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
   });
 };
