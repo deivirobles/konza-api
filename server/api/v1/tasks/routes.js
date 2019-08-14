7c28f5c860f580538ee4e9ad3f464756c48c249d
Deivi Robles <drozam24@gmail.com>
2019-08-14 10:13:03 -0500

    Separate Tasks routes and prefix them

--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const tasks = require('./tasks/routes');
 
-router.route('/tasks').get((req, res, next) => {
+router.get('/', (req, res, next) => {
   res.json({
     message: 'GET all tasks',
   });
 });
-
-router.use('/tasks', tasks);
 
 module.exports = router;
